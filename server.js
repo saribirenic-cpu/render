449d9b3c260ca865858752f7145e1b8700f97611
saribirenic-cpu <sarib.irenic@gmail.com>
2025-10-07 16:58:45 +0500

    added data into employyee.json

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const filePath = "./employees.json";
+const filePath = "api-key-server/employees.json";
 const apiKeys = [];
 
 // Load existing employees
